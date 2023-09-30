599bea8e36492fac14f8230081cd7d35c757596e
William Alievi <william@welvind.com>
2023-09-30 10:22:27 -0300

    Base

---
+++ prod_preco/src/main/java/com/walievi/Main.java
@@ -0,0 +1,11 @@
+package com.walievi;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Produto produto = new Produto();
+
+    }
+}
---
+++ prod_preco/src/main/java/com/walievi/Produto.java
@@ -0,0 +1,5 @@
+package com.walievi;
+
+public class Produto {
+    
+}
